732e73666e938533ea340bd254435c01832cd604
miabarada <mia.barada2005@gmail.com>
2025-12-07 20:13:38 +0100

    visitor, ticket, performer added

--- tables.sql
+++ tables.sql
@@ -41,3 +41,64 @@ ALTER TABLE performer
 	ADD CONSTRAINT ValidPerformerType CHECK (PerformerType IN ('band', 'DJ', 'solo')),
 	ADD CONSTRAINT PositiveNumberOfMembers CHECK (NumberOfMembers > 0);
 
+CREATE TABLE performerAtFestival (
+	FestivalId INT REFERENCES festival(FestivalId), 
+	PerformerId INT REFERENCES performer(PerformerId)
+);
+
+CREATE TABLE performance (
+	BeginningTime TIMESTAMP NOT NULL,
+	EndTime TIMESTAMP NOT NULL,
+	NumberOfVisitors INT,
+	FestivalId INT REFERENCES festival(FestivalId),
+	StageId INT REFERENCES stage(StageId),
+	PerformerId INT REFERENCES performer(performerId)
+);
+
+CREATE FUNCTION validPerformance()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF EXISTS (
+		SELECT 1 FROM performance p
+        WHERE p.StageId = NEW.StageId
+          AND p.PerformanceId <> NEW.PerformanceId
+          AND NEW.StartTime < p.EndTime
+          AND NEW.EndTime > p.StartTime
+    ) THEN
+        RAISE EXCEPTION 'Overlaping performances';
+    END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trgValidPerformance
+BEFORE INSERT ON performance
+FOR EACH ROW
+EXECUTE FUNCTION validPerformance();
+
+CREATE TABLE ticket(
+	TicketId SERIAL PRIMARY KEY,
+	Type VARCHAR(100),
+	Price DECIMAL(10, 2),
+	Description VARCHAR(200),
+	Validity VARCHAR(100),
+	FestivalId INT REFERENCES festival(FestivalId)
+);
+
+ALTER TABLE ticket
+	ADD CONSTRAINT ValidType CHECK(Type IN('daily', 'whole festival', 'VIP', 'camp')),
+	ADD CONSTRAINT PositivePrice CHECK(Price > 0),
+	ADD CONSTRAINT Validity CHECK(Validity IN ('daily', 'whole festival'));
+
+CREATE TABLE visitor(
+	VisitorId SERIAL PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL,
+	Surname VARCHAR(100) NOT NULL,
+	DateOfBirth DATE,
+	City VARCHAR(100),
+	Email VARCHAR(100) UNIQUE,
+	Country VARCHAR(100)
+);
+
+
+
